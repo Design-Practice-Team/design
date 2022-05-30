a95560ce5c5df74eb128bec825be21dd165f5ea3
fujitako03 <thisis.snsd03@gmail.com>
2022-05-30 18:05:38 +0900

    mod 選択済み商品の情報をオプションにも対応

--- AP_H31_Spring_PM03/fujita/main.py
+++ AP_H31_Spring_PM03/fujita/main.py
@@ -3,7 +3,7 @@ import pprint
 def main():
     main_menu = input("メインを入力: ")
     pprint.pprint({
-        "選択済み商品：": [main_menu],
+        "選択済み商品：": [{"type": "main", "name": main_menu, "option": "***"}],
         "直前のイベントコード：": "***",
         "状態番号": "***",
         "注文金額": "***",
@@ -13,7 +13,10 @@ def main():
     
     side_menu_1 = input("サイド1を入力:")
     pprint.pprint({
-        "選択済み商品：": [main_menu, side_menu_1],
+        "選択済み商品：": [
+            {"type": "main", "name": main_menu, "option": "***"},
+            {"type": "side_1", "name": side_menu_1, "option": "***"},
+            ],
         "直前のイベントコード：": "***",
         "状態番号": "***",
         "注文金額": "***",
@@ -23,7 +26,11 @@ def main():
 
     side_menu_2 = input("サイド2を入力: ")
     pprint.pprint({
-        "選択済み商品：": [main_menu, side_menu_1, side_menu_2],
+        "選択済み商品：": [
+            {"type": "main", "name": main_menu, "option": "***"},
+            {"type": "side_1", "name": side_menu_1, "option": "***"},
+            {"type": "side_2", "name": side_menu_2, "option": "***"},
+            ],
         "直前のイベントコード：": "***",
         "状態番号": "***",
         "注文金額": "***",
